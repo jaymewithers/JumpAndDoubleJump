438bbb602c4b993cb3bec50fdea155fd101248da
Jayme Withers <10859571@my.uvu.edu>
2020-10-05 15:59:33 -0600

    jump and double jump

---
+++ Assets/Scripts/CharacterJump.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterJump : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+
+    public float gravity = -9.81f, jumpForce = 30f;
+    private float yVar;
+
+    public IntData playerJumpCount;
+    private int jumpCount;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    private void Update()
+    {
+        yVar += gravity * Time.deltaTime;
+        movement.Set(0, yVar, 0);
+
+        if (controller.isGrounded && movement.y < 0)
+        {
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < playerJumpCount.value)
+        {
+            yVar = jumpForce;
+            jumpCount++;
+        }
+        
+        controller.Move(movement* Time.deltaTime);
+    }
+}
